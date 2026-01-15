f536a0d65d07a9e49873b2e6780bb86212ed41c0
markstt <cepulapu79@gmail.com>
2026-01-15 09:24:50 +0200

    Create python_datu_tipi_numbers_strings.py

---
+++ python_datu_tipi_numbers_strings.py
@@ -0,0 +1,48 @@
+#15/01/2026
+#mainigie
+#numbers - skaitļi
+""""
+vairaka liniju komentars
+
+
+"""
+
+"""
+Definē divus skaitļus
+Veic četras matemātiskās darbības ar tiem - +,-,/,*
+Izvadi katru rezultātu
+"""
+
+#Define divus skaitlurs
+
+# datu tips ir int
+skaitlis1 = 5 
+print(skaitlis1)
+skaitlis2 = 7
+print(skaitlis2)
+
+print(skaitlis1 + skaitlis2)
+print(skaitlis1 - skaitlis2)
+print(skaitlis1 / skaitlis2)
+print(skaitlis1 * skaitlis2)
+ # string - tekst/simbolu virkne
+"""
+✍️ Praktiskais uzdevums
+Definē divus teksta mainīgos
+Izvadi tos atsevišķi un kopā
+Nosaki simbolu skaitu
+Apgriez tekstu otrādāk
+Izvadīt 1 teksta simbolu
+Izvadīt 2 un 3 teksta simbolu
+"""
+
+teksts1 = "5"
+teksts2 = "gadi"
+#Izvadi tos atsevišķi un kopā
+print(teksts1)
+print(teksts2)
+print(teksts1 + teksts2)
+print(teksts1, teksts2)
+#Nosaki simbolu skaitu
+print(len(teksts1))
+print(len(teksts2))

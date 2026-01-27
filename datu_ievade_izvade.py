0853adaf4e58d78b052fa84712f589667694b1eb
markstt <cepulapu79@gmail.com>
2026-01-27 09:36:25 +0200

    datu_ievade_izvade

---
+++ datu_ievade_izvade.py
@@ -0,0 +1,21 @@
+#27/01/2026
+#Lietotaja ievade un izvade Python
+
+#Lietotaja input()
+#vards = input("Ievadi savu vardu: ")
+#print(vards)
+#vecums = int(input("Ievadi savu vecumu: "))
+#print("Nakoš gad tev būs:", vecums + 1)
+#pievienot vertibu sarakstam
+#prieksmeti = []
+#prieksmeti.append(input("Ievadi pirmo priekšmetu: "))
+#prieksmeti.append(input("Ievadi otro priekšmetu: "))
+
+#print("Tavi priekšmeti ir:", prieksmeti)
+
+#Vardniva - pievienot vertibu vardnicai un izvadit tas
+skolens = {}
+skolens["vards"] = input("Ievadi skolēna vārdu: ")
+skolens["vecums"] = int(input("Ievadi skolēna vecumu: "))
+#Vardnicas izvade
+print("Skolēna informācija:", skolens)
